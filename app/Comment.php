0fc2a482be332a3803da2bce31f1dba59aec8a3f
kleilike <kleilike@gmail.com>
2018-01-16 14:35:58 +0300

    sluggable

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Comment extends Model
 {
+    use Sluggable;
+
     public function post()
     {
         return $this->hasOne(Post::class);
@@ -14,5 +17,14 @@ class Comment extends Model
     public function author()
     {
         return $this->hasOne(User::class);
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
+
     public function category()
     {
         return $this->hasOne(Category::class);
@@ -30,5 +33,14 @@ class Post extends Model
             'post_id',
             'tag_id'
         );
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
