0b130364fbeb5eb4fc640a7bbce9936f0ea2176e
kleilike <kleilike@gmail.com>
2018-01-16 12:43:26 +0300

    relationships

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    /**
+     * Tag - модель с которой связываемся
+     * post_tag - таблица для связи многое-ко-многим
+     * post_id - id этой модели в post_tag
+     * tag_id - id модели Tag
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'posts_tags',
+            'post_id',
+            'tag_id'
+        );
+    }
 }
